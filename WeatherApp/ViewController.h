2a746042ec4690f206f4e62d23791301fd1e4472
Halkyonbird <alex.sheverdin@gmail.com>
2015-08-09 17:04:01 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by User on 09.08.15.
+//  Copyright (c) 2015 Alex Sheverdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeatherApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeatherApp
+//
+//  Created by User on 09.08.15.
+//  Copyright (c) 2015 Alex Sheverdin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
