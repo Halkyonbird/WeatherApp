640f9ef98ccc81cbd6566ef31d79346cbc6523c5
Halkyonbird <alex.sheverdin@gmail.com>
2015-08-10 23:19:35 +0300

    first commit

---
+++ WeatherApp/TableViewController.m
@@ -0,0 +1,113 @@
+//
+//  TableViewController.m
+//  WeatherApp
+//
+//  Created by User on 09.08.15.
+//  Copyright (c) 2015 Alex Sheverdin. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+- (void)refreshTable {
+    
+    [self.tableView reloadData];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    NSUInteger count = [self.forcast count];
+    return count;
+    //return [self.forcast count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                           reuseIdentifier:@"UITableViewCell"];
+    
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    
+    //cell.textLabel.text = [self.portfolio.holdings[indexPath.row] description];
+    NSDictionary * dict = self.forcast[indexPath.row];
+    NSString * cellText = [dict valueForKey:@"dt_txt"];
+    cell.textLabel.text = cellText;
+  
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.m
 //  WeatherApp
 //
@@ -7,21 +7,157 @@
 //
 
 #import "ViewController.h"
+#import "ASIHTTPRequest.h"
+#import "TableViewController.h"
+//&units=metric
+static NSString  *urlWeather = @"http://api.openweathermap.org/data/2.5/weather?lat=50&lon=36.25&units=metric";
+static NSString  *urlForecast = @"http://api.openweathermap.org/data/2.5/forecast?q=kharkiv";
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSDictionary *allWeatherData;
+@property (nonatomic, strong) NSArray *forcast;
+@property (nonatomic, weak) TableViewController *tableViewController;
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+@property (weak, nonatomic) IBOutlet UILabel *tempLabel;
 
 @end
 
 @implementation ViewController
 
+
+- (IBAction)getCurrentLocation:(id)sender {
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  
+    [self.locationManager startUpdatingLocation];
+}
+
+
+- (IBAction)refresh:(UIButton *)sender {
+
+    [self downloadWeather];
+    [self downloadForecast];
+    //NSLog(@"asdf: %@", self.forcast);
+
+}
+
+- (void) downloadWeather {
+
+    [self downloadWeatherDataFromURL:urlWeather withBlock:^(id result) {
+        if ([result isKindOfClass:[NSError class]]) {
+            //
+        } else if ([result isKindOfClass:[NSData class]]) {
+            NSError *error;
+            self.allWeatherData = [NSJSONSerialization JSONObjectWithData:result options:0 error:&error];
+            //self.forcast = [self.allWeatherData valueForKey:@"list"];
+            NSDictionary *dict = [self.allWeatherData valueForKey:@"main"];
+            self.tempLabel.text = [NSString stringWithFormat:@"%@º", [dict valueForKey:@"temp"]];
+//            NSLog(@"Data = %@", self.allWeatherData);
+            NSLog(@"Data = %@", dict);
+        }
+        
+    }];
+}
+
+- (void) downloadForecast {
+    
+    [self downloadWeatherDataFromURL:urlForecast withBlock:^(id result) {
+        if ([result isKindOfClass:[NSError class]]) {
+            //
+        } else if ([result isKindOfClass:[NSData class]]) {
+            NSError *error;
+            self.allWeatherData = [NSJSONSerialization JSONObjectWithData:result options:0 error:&error];
+            self.forcast = [self.allWeatherData valueForKey:@"list"];
+            
+            
+            self.tableViewController.forcast = self.forcast;
+            [self.tableViewController refreshTable];
+        }
+        
+    }];
+
+}
+
+- (void)downloadWeatherDataFromURL:(NSString *) urlString withBlock:(void(^)(id result))completion {
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    ASIHTTPRequest *request = [[ASIHTTPRequest alloc] initWithURL:url];
+    __weak typeof(request) wRequest = request;
+    [request setCompletionBlock:^{
+        if (completion) {
+            completion(wRequest.responseData);
+        }
+    }];
+    
+    [request setFailedBlock:^{
+        if (completion) {
+            completion(wRequest.error);
+        }
+    }];
+    
+//    [request setBytesReceivedBlock:^(unsigned long long size, unsigned long long total) {
+//        NSLog(@"Progress: %llu", size/total);
+//    }];
+    
+    [request startAsynchronous];
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if ([segue.identifier isEqualToString:@"ToTable"]) {
+        if ([segue.destinationViewController isKindOfClass:[TableViewController class]]) {
+            self.tableViewController = (TableViewController *)segue.destinationViewController;
+            self.tableViewController.forcast = self.forcast;
+        }
+
+    }
+}
+
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+//- (void)locationManager:(CLLocationManager *)manager
+//     didUpdateLocations:(NSArray*)locations {
+//    
+//    NSLog(@"locs^ %@", locations);
+//}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    //NSLog(@"didUpdateToLocation: %@", newLocation);
+    
+    CLLocation *currentLocation = newLocation;
+    
+    if (currentLocation != nil) {
+        _longitudeLabel = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+        _latitudeLabel = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+    }
+
+ 
+}
+
+#pragma mark - Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+    self.locationManager = [[CLLocationManager alloc] init];}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
